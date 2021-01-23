2a2a0ef4997a69650d7210ec6cc932a1e688ce90
joshrpg <xjoshrpgx@gmail.com>
2021-01-22 22:11:35 -0600

    No need to notice

--- src/js/background.js
+++ src/js/background.js
@@ -1184,66 +1184,67 @@ var tgs = (function () {
     }
 
     function checkForNotices() {
-        gsUtils.log('background', 'Checking for notices..');
-        var xhr = new XMLHttpRequest();
-        var lastShownNoticeVersion = gsStorage.fetchNoticeVersion();
+        // gsUtils.log('background', 'Checking for notices..');
+        // var xhr = new XMLHttpRequest();
+        // var lastShownNoticeVersion = gsStorage.fetchNoticeVersion();
 
-        xhr.open('GET', 'https://greatsuspender.github.io/notice.json', true);
-        xhr.timeout = 4000;
-        xhr.setRequestHeader('Cache-Control', 'no-cache');
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.responseText) {
-                var resp;
-                try {
-                    resp = JSON.parse(xhr.responseText);
-                } catch (e) {
-                    gsUtils.error(
-                        'background',
-                        'Failed to parse notice response',
-                        xhr.responseText
-                    );
-                    return;
-                }
+        // xhr.open('GET', 'https://greatsuspender.github.io/notice.json', true);
+        // xhr.timeout = 4000;
+        // xhr.setRequestHeader('Cache-Control', 'no-cache');
+        // xhr.onreadystatechange = function() {
+        //   if (xhr.readyState === 4 && xhr.responseText) {
+        //     var resp;
+        //     try {
+        //       resp = JSON.parse(xhr.responseText);
+        //     } catch (e) {
+        //       gsUtils.error(
+        //         'background',
+        //         'Failed to parse notice response',
+        //         xhr.responseText
+        //       );
+        //       return;
+        //     }
 
-                if (!resp || !resp.active || !resp.text) {
-                    gsUtils.log('background', 'No new notice found');
-                    return;
-                }
+        //     if (!resp || !resp.active || !resp.text) {
+        //       gsUtils.log('background', 'No new notice found');
+        //       return;
+        //     }
 
-                //only show notice if it is intended for this extension version
-                var noticeTargetExtensionVersion = String(resp.target);
-                if (
-                    noticeTargetExtensionVersion !== chrome.runtime.getManifest().version
-                ) {
-                    gsUtils.log(
-                        'background',
-                        `Notice target extension version: ${noticeTargetExtensionVersion}
-            does not match actual extension version: ${chrome.runtime.getManifest().version
-                        }`
-                    );
-                    return;
-                }
+        //     //only show notice if it is intended for this extension version
+        //     var noticeTargetExtensionVersion = String(resp.target);
+        //     if (
+        //       noticeTargetExtensionVersion !== chrome.runtime.getManifest().version
+        //     ) {
+        //       gsUtils.log(
+        //         'background',
+        //         `Notice target extension version: ${noticeTargetExtensionVersion}
+        //         does not match actual extension version: ${
+        //           chrome.runtime.getManifest().version
+        //         }`
+        //       );
+        //       return;
+        //     }
 
-                //only show notice if it has not already been shown
-                var noticeVersion = String(resp.version);
-                if (noticeVersion <= lastShownNoticeVersion) {
-                    gsUtils.log(
-                        'background',
-                        `Notice version: ${noticeVersion} is not greater than last shown notice version: ${lastShownNoticeVersion}`
-                    );
-                    return;
-                }
+        //     //only show notice if it has not already been shown
+        //     var noticeVersion = String(resp.version);
+        //     if (noticeVersion <= lastShownNoticeVersion) {
+        //       gsUtils.log(
+        //         'background',
+        //         `Notice version: ${noticeVersion} is not greater than last shown notice version: ${lastShownNoticeVersion}`
+        //       );
+        //       return;
+        //     }
 
-                //show notice - set global notice field (so that it can be trigger to show later)
-                _noticeToDisplay = resp;
-                gsAnalytics.reportEvent(
-                    'Notice',
-                    'Prep',
-                    resp.target + ':' + resp.version
-                );
-            }
-        };
-        xhr.send();
+        //     //show notice - set global notice field (so that it can be trigger to show later)
+        //     _noticeToDisplay = resp;
+        //     gsAnalytics.reportEvent(
+        //       'Notice',
+        //       'Prep',
+        //       resp.target + ':' + resp.version
+        //     );
+        //   }
+        // };
+        // xhr.send();
     }
 
     function requestNotice() {
@@ -1910,7 +1911,5 @@ Promise.resolve()
         gsUtils.error('background init error: ', error);
     })
     .finally(() => {
-        gsAnalytics.performStartupReport();
-        gsAnalytics.performVersionReport();
         tgs.startTimers();
     });
